1e84c614b709c505c85daecc4c7032a0a4b3b263
Marco Behnke <marco@behnke.biz>
2022-07-26 10:34:42 +0200

    build settings for play mode tests

---
+++ Assets/Tests/PlayMode/PlayModeTestRunCallback.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using NUnit.Framework.Interfaces;
+using UnityEngine;
+using UnityEngine.TestRunner;
+
+[assembly: TestRunCallback(typeof(PlayModeTestRunCallback))]
+
+public class PlayModeTestRunCallback : ITestRunCallback
+{
+    public void RunStarted(ITest testsToRun)
+    {
+    }
+
+    public void RunFinished(ITestResult testResults)
+    {
+        var result = testResults.ToXml(true);
+        var path = Path.GetFullPath(Path.Combine(Application.dataPath, ".//result.xml"));
+        var writer = new StreamWriter(path, false);
+        writer.Write(
+            PrettyXml(
+                result.OuterXml
+            )
+        );
+        writer.Close();
+    }
+
+    public void TestStarted(ITest test)
+    {
+    }
+
+    public void TestFinished(ITestResult result)
+    {
+        if (!result.Test.IsSuite) Debug.Log($"Result of {result.Name}: {result.ResultState.Status}");
+    }
+
+    private static string PrettyXml(string xml)
+    {
+        var stringBuilder = new StringBuilder();
+
+        var element = XElement.Parse(xml);
+
+        var settings = new XmlWriterSettings();
+        settings.OmitXmlDeclaration = true;
+        settings.Indent = true;
+
+        using (var xmlWriter = XmlWriter.Create(stringBuilder, settings))
+        {
+            element.Save(xmlWriter);
+        }
+
+        return stringBuilder.ToString();
+    }
+}
